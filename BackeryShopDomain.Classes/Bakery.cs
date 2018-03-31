1dda9919a0f76d38cb5b58fd43895438562c1ca5
Ivan Rakovic <ivan.rakovic@mogul.com>
2018-03-31 17:25:55 +0200

    remove unnecessary directives

--- BackeryShopDomain.Classes/Bakery.cs
+++ BackeryShopDomain.Classes/Bakery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BackeryShopDomain.Classes
+﻿namespace BackeryShopDomain.Classes
 {
 	public class Bakery
 	{
