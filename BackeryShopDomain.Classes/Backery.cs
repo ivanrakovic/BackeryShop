9f087dfa14563fa6f7a0289b02311d23237683c9
Ivan Rakovic <ivan.rakovic@mogul.com>
2018-04-01 14:53:56 +0200

    Validation added

--- BackeryShopDomain.Classes/Backery.cs
+++ BackeryShopDomain.Classes/Backery.cs
@@ -1,12 +1,22 @@
-﻿namespace BackeryShopDomain.Classes
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackeryShopDomain.Classes
 {
 	public class Backery
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		public int NumberOfShifts { get; set; }
 
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+		[Index("IX_BakeryName", IsUnique = true)]
+		public string Name { get; set; }
+
+		[Range(1,3)]
+		public int NumberOfShifts { get; set; }
+		
 		public PriceList PriceList { get; set; }
+
+		[Required]
 		public int PriceListId { get; set; }
 	}
 }
