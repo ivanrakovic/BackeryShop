e293ed2002df0be1a37a1e39e435188efd783861
Ivan Raković <ivan@geta.no>
2018-05-10 16:16:56 +0200

    Default value to be empty

--- BackeryShop.Web/Controllers/PriceListDetailsController.cs
+++ BackeryShop.Web/Controllers/PriceListDetailsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using BackeryShopDomain.Classes;
 using BackeryShopDomain.DataModel;
 
