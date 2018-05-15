89357b757d08c452507ba884860a572dd9ec0dbe
Ivan Raković <ivan@geta.no>
2018-05-15 11:47:20 +0200

    DatePicker added - inital commit

--- BackeryShop.Web/App_Start/BundleConfig.cs
+++ BackeryShop.Web/App_Start/BundleConfig.cs
@@ -13,10 +13,11 @@ namespace BackeryShop.Web
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
-
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+		    bundles.Add(new ScriptBundle("~/bundles/backery").Include(
+                "~/ClientUI/js/backery*"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
---
+++ BackeryShop.Web/ClientUI/js/backery.turnover.js
@@ -0,0 +1,16 @@
+﻿var Backery = Backery || {};
+
+Backery.turnover = (function ($) {
+
+    var me = {};
+
+    $('#turnover-date-input').datepicker({
+        format: "dd/mm/yyyy",
+        weekStart: 1,
+        todayBtn: "linked",
+        language: "rs-latin"
+    });
+
+
+    return me;
+}(jQuery));
