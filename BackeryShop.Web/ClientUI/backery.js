a05dee4ebd5f704e7118453742cdf81407a6ddb7
Ivan Raković <ivan@geta.no>
2018-05-15 10:40:55 +0200

    JS added

---
+++ BackeryShop.Web/ClientUI/backery.js
@@ -0,0 +1,10 @@
+﻿var Backery = Backery || {};
+
+// Auto-initialization of modules
+jQuery(function () {
+    for (var key in Backery) {
+        if (typeof Backery[key]._initialize == 'function') {
+            Backery[key]._initialize.call(Backery[key]);
+        }
+    }
+});
