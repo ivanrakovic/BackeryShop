33bfc23d011b53533f6a13a21bbf08bb99ae9289
Ivan Raković <ivan@geta.no>
2018-05-16 12:54:41 +0200

    small refactor

--- BackeryShop.Web/Controllers/TurnoverDataController.cs
+++ BackeryShop.Web/Controllers/TurnoverDataController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using BackeryShop.Web.Models.ViewModels;
+using BackeryShop.Web.Services;
 using BackeryShopDomain.DataModel;
 
 namespace BackeryShop.Web.Controllers
@@ -14,24 +11,16 @@ namespace BackeryShop.Web.Controllers
         public ActionResult Create(int id)
         {
             var model = new TurnoverViewModel();
-            
+
             using (var db = new BackeryContext())
             {
                 var backery = db.Backeries.Find(id);
                 model.Backery = backery;
-                model.TurnoverProductsViewModels = (from b in db.Backeries
-                    join pl in db.PriceLists on b.PriceListId equals pl.Id
-                    join pld in db.PriceListDetails on pl.Id equals pld.PriceListId
-                    join p in db.Products on pld.ProductId equals p.Id
-                    where b.Id == id
-                    select new TurnoverProductViewModel
-                    {
-                        DisplayName = p.Name,
-                        ProdtId = p.Id,
-                        Price = pld.Price
-                    }).ToList();
+                model.TurnoverProductsViewModels = TurnoverService.GetTurnoversForBackery(backery);
             };
             return View(model);
         }
+
+        
     }
 }
---
+++ BackeryShop.Web/Services/TurnoverService.cs
@@ -0,0 +1,38 @@
+﻿using BackeryShop.Web.Models.ViewModels;
+using BackeryShopDomain.Classes;
+using BackeryShopDomain.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackeryShop.Web.Services
+{
+    public static class TurnoverService
+    {
+        public static List<TurnoverProductViewModel> GetTurnoversForBackery(Backery backery)
+        {
+            return GetTurnoversForBackeryId(backery.Id);
+        }
+
+        public static List<TurnoverProductViewModel> GetTurnoversForBackeryId(int id)
+        {
+            var result = new List<TurnoverProductViewModel>();
+            using (var db = new BackeryContext())
+            {
+                result = (from b in db.Backeries
+                          join pl in db.PriceLists on b.PriceListId equals pl.Id
+                          join pld in db.PriceListDetails on pl.Id equals pld.PriceListId
+                          join p in db.Products on pld.ProductId equals p.Id
+                          where b.Id == id
+                          select new TurnoverProductViewModel
+                          {
+                              DisplayName = p.Name,
+                              ProdtId = p.Id,
+                              Price = pld.Price
+                          }).ToList();
+            }
+            return result;
+        }
+    }
+}
