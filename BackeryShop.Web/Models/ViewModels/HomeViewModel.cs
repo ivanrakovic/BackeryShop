bd526afa6a8ce614cce931589372d6176ca0e537
ivanrakovic <ivan@geta.no>
2018-05-19 15:54:29 +0200

    remove views and add new viewmodel

---
+++ BackeryShop.Web/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackeryShop.Web.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<int> BackeriesList { get; set; }
+    }
+}
