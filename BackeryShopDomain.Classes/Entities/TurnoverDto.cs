6f0e88ea262b0ad5f9a9a341dff6d8fb189b4a66
Ivan Raković <ivan@geta.no>
2018-05-21 14:23:47 +0200

    Dtos added

---
+++ BackeryShopDomain.Classes/Entities/TurnoverDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackeryShopDomain.Classes.Entities
+{
+    public class TurnoverDto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Datum")]
+        public DateTime Date { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        [Display(Name = "Smena")]
+        public int ShiftNo { get; set; }
+        [Display(Name = "Pekara")]
+        public int BackeryId { get; set; }
+        public IEnumerable<TurnoverDetailDto> TurnoverDetails { get; set; }
+    }
+}
